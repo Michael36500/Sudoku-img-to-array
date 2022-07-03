5235744457b7ec70ddca3efc90bb8f80854ea0c5
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-07-03 10:24:51 +0200

    added OCR and some error handling

--- read_img.py
+++ read_img.py
@@ -1,14 +1,19 @@
-from logging import raiseExceptions
 import pytesseract
 from PIL import Image
 
 
 def read_img(path):
-    text = pytesseract.image_to_string(Image.open(path))
-    if text == "":
+    text = pytesseract.image_to_string(Image.open(path), config="--psm 13")
+    # print(text)
+    # print(len(text))
+    # if len(text) != 1:
+    #     print("tesseract detect less/more than one char")
+    if "—" in text:
         text = 0
-    if text != "0" or text != "1" or text != "2" or text != "3" or text != "4" or text != "5" or text != "6" or text != "7" or text != "8" or text != "9":
-        print("Tesseract detected characters, not ints")
+    # if not text in list('0123456789'):
+    #     print("Tesseract detected characters, not ints")
+    if not str(text) in list("0123456789"):
+        print("tesseract detected non ints")
     return text
 
-# read_img("test.png")
+print(read_img("hidden_board.png"))
